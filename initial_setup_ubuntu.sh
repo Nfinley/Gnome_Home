3cc2c51bc03caeccc7990c1653da35702d03c874
Nigel Finley <nfinleymusic@gmail.com>
2016-11-10 21:30:58 -0600

    adding ubuntu shell script

---
+++ initial_setup_ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+sudo npm install -g sequelize-cli
+sudo npm install -g sails
+
+
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc
+exec $SHELL
+
+rbenv install 2.3.1
+rbenv global 2.3.1
+ruby -v
+
+gem install overcommit
