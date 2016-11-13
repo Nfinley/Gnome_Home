df5c507efbff48922542d0db5e7859e462543575
Matt Holland <mtholland10@gmail.com>
2016-11-13 14:09:04 -0600

    Integrate heroku db

---
+++ config/connections.js
@@ -0,0 +1,106 @@
+/**
+ * Connections
+ * (sails.config.connections)
+ *
+ * `Connections` are like "saved settings" for your adapters.  What's the difference between
+ * a connection and an adapter, you might ask?  An adapter (e.g. `sails-mysql`) is generic--
+ * it needs some additional information to work (e.g. your database host, password, user, etc.)
+ * A `connection` is that additional information.
+ *
+ * Each model must have a `connection` property (a string) which is references the name of one
+ * of these connections.  If it doesn't, the default `connection` configured in `config/models.js`
+ * will be applied.  Of course, a connection can (and usually is) shared by multiple models.
+ * .
+ * Note: If you're using version control, you should put your passwords/api keys
+ * in `config/local.js`, environment variables, or use another strategy.
+ * (this is to prevent you inadvertently sensitive credentials up to your repository.)
+ *
+ * For more information on configuration, check out:
+ * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.connections.html
+ */
+
+module.exports.connections = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Local disk storage for DEVELOPMENT ONLY                                  *
+  *                                                                          *
+  * Installed by default.                                                    *
+  *                                                                          *
+  ***************************************************************************/
+  localDiskDb: {
+    adapter: 'sails-disk'
+  },
+
+  /***************************************************************************
+  *                                                                          *
+  * MySQL is the world's most popular relational database.                   *
+  * http://en.wikipedia.org/wiki/MySQL                                       *
+  *                                                                          *
+  * Run: npm install sails-mysql                                             *
+  *                                                                          *
+  ***************************************************************************/
+  someMysqlServer: {
+    adapter: 'sails-mysql',
+    /* The below is the previous local info:
+    host: 'localhost',
+    user: 'root', //optional
+    password: 'Trooper2#', //optional
+    database: 'gnome_db' //optional
+    */
+    /* Below is the new heroku info: */
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'b37ba81f05baf8',
+    password: '305ed91d',
+    database: 'heroku_0e2ec19a9328d42'
+    // database: heroku_0e2ec19a9328d42
+    /* Note: everything after the @ symbol until / is the db host: us-cdbr-iron-east-04.cleardb.net
+     * Everything after the / until ? is the db name: heroku_0e2ec19a9328d42
+     * Everything after the // until : is the db username: b37ba81f05baf8
+     * The string between : and @ is the db password: 305ed91d
+     */
+    // CLEARDB_DATABASE_URL: mysql://b37ba81f05baf8:305ed91d@us-cdbr-iron-east-04.cleardb.net/heroku_0e2ec19a9328d42?reconnect=true
+  },
+
+  /***************************************************************************
+  *                                                                          *
+  * MongoDB is the leading NoSQL database.                                   *
+  * http://en.wikipedia.org/wiki/MongoDB                                     *
+  *                                                                          *
+  * Run: npm install sails-mongo                                             *
+  *                                                                          *
+  ***************************************************************************/
+  // someMongodbServer: {
+  //   adapter: 'sails-mongo',
+  //   host: 'localhost',
+  //   port: 27017,
+  //   user: 'username', //optional
+  //   password: 'password', //optional
+  //   database: 'your_mongo_db_name_here' //optional
+  // },
+
+  /***************************************************************************
+  *                                                                          *
+  * PostgreSQL is another officially supported relational database.          *
+  * http://en.wikipedia.org/wiki/PostgreSQL                                  *
+  *                                                                          *
+  * Run: npm install sails-postgresql                                        *
+  *                                                                          *
+  *                                                                          *
+  ***************************************************************************/
+  // somePostgresqlServer: {
+  //   adapter: 'sails-postgresql',
+  //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
+  //   user: 'YOUR_POSTGRES_USER', // optional
+  //   password: 'YOUR_POSTGRES_PASSWORD', // optional
+  //   database: 'YOUR_POSTGRES_DB' //optional
+  // }
+
+
+  /***************************************************************************
+  *                                                                          *
+  * More adapters: https://github.com/balderdashy/sails                      *
+  *                                                                          *
+  ***************************************************************************/
+
+};
